cc27c81b3764add5e4e4b6f0a9747d62dbf281fb
EncryptEx <limusina10@gmail.com>
2023-10-23 12:01:03 +0200

    Shrink dashboard links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,8 +12,13 @@
             @foreach ($links as $link)
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-3">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h4 class="text-2xl font-medium text-white title-font mb-2">{{ $link['long_url'] }} </h4>
-                    <p class="leading-relaxed">{{ route('redirect' ,$link['short_url']) }}</p>
+                    <h4 class="text-2xl font-medium text-white title-font mb-2">{{ route('redirect' ,$link['short_url']) }} </h4>
+                    <p class="leading-relaxed">@if (strlen($link['long_url']) > 50):
+
+    {{ substr($yourString, 0, 50) }}
+@else
+    {{ $link['long_url'] }}
+@endif</p>
                     <a class="text-indigo-400 inline-flex items-center mt-4" href="{{ route('redirect' ,$link['short_url']) }}">Visit link
                     <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M5 12h14"></path>
