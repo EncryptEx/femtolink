6d04aeef99a385e357c98a39f25027bca0bbd783
EncryptEx <limusina10@gmail.com>
2024-07-07 12:30:21 +0200

    feat: added link to docs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,10 +97,11 @@
       </div>
       <div class="text-center">
         <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Shrinking links was never that easy</h1>
-        <p class="mt-6 text-lg leading-8 text-gray-100">Start now with a fully fuctional API for link shrinking ready for intense usage. You can register in less than a minute.</p>
+        <p class="mt-6 text-lg leading-8 text-gray-100">Start now with a fully fuctional API for link shrinking ready for intense usage.
+          <br> You can register in less than a minute.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <a href="{{ route('register') }}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-          <a href="#" class="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
+          <a href="/api/docs" class="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
         </div>
       </div>
     </div>
