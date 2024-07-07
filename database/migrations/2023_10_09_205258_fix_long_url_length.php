20d3f9be6d9092394d61f00a2f300fb1a2063c18
EncryptEx <limusina10@gmail.com>
2024-07-07 11:03:52 +0200

    fix: migration error

--- database/migrations/2023_10_09_205258_fix_long_url_length.php
+++ database/migrations/2023_10_09_205258_fix_long_url_length.php
@@ -10,9 +10,25 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::table('links', function ($table) {
-            $table->longText('long_url')->change();
+        // Rename the original table
+        Schema::rename('links', 'temp_table_name');
+
+        // Create the new table with the updated column type
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ownerId');
+            $table->longText('long_url');
+            $table->string('short_url');
+            $table->timestamps();
         });
+
+        // Copy data from the old table to the new table
+        DB::table('links')->insert(
+            DB::table('temp_table_name')->select('id', 'ownerId', 'long_url', 'short_url', 'created_at', 'updated_at')->get()->toArray()
+        );
+
+        // Drop the temporary table
+        Schema::drop('temp_table_name');
     }
 
     /**
@@ -20,8 +36,24 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::table('links', function ($table) {
-            $table->string('long_url')->change();
+        // Rename the new table to temporary table
+        Schema::rename('links', 'temp_table_name');
+
+        // Recreate the original table with the original column type
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ownerId');
+            $table->string('long_url');
+            $table->string('short_url');
+            $table->timestamps();
         });
+
+        // Copy data back from the temp table to the original table
+        DB::table('links')->insert(
+            DB::table('temp_table_name')->select('id', 'column_name', 'other_column', 'created_at', 'updated_at')->get()->toArray()
+        );
+
+        // Drop the temporary table
+        Schema::drop('temp_table_name');
     }
 };
